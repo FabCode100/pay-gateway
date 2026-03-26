adba709c173bea0af656a6a9fd7bcab842e3652b
FabCode100 <fabriciobc47@gmail.com>
2026-03-26 14:26:14 -0300

    frontend deploy correct

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Adicione esta linha
+  images: {
+    unoptimized: true, // Necessário se você usa o componente <Image /> do Next
+  },
 };
 
 module.exports = nextConfig;
